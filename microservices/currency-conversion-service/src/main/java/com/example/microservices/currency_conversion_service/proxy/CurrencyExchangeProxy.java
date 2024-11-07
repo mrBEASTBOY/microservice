faf32f33977a7692828e01acf8dac801e699c6e1
mrBEASTBOY <hlinh0409@gmail.com>
2024-11-06 21:50:06 -0600

    adding currency-conversion-service and restTemplate and Feign

---
+++ microservices/currency-conversion-service/src/main/java/com/example/microservices/currency_conversion_service/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,20 @@
+package com.example.microservices.currency_conversion_service.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.microservices.currency_conversion_service.bean.CurrencyConversion;
+
+@FeignClient(name="currency-exchange", url="localhost:8000")
+public interface CurrencyExchangeProxy {
+	// Copy the method that we want to 
+	// call from other service (the method 
+	// signature must be same
+	@GetMapping("currency-exchange/from/{from}/to/{to}")
+	  public CurrencyConversion retrieveExchangeValue(
+	    @PathVariable String from,
+	    @PathVariable String to
+	  );
+}
+
